74bb6372935a39ae5985b04a38734ef764a29980
Artem <113770041+Artemdream@users.noreply.github.com>
2023-01-27 15:09:56 +0200

    update video player

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -5,9 +5,16 @@ const iframe = document.querySelector("iframe");
 const player = new Player(iframe);
 
 player.on('timeupdate', throttle(function (time) {
-    localStorage.setItem("videoplayer-current-time", time.seconds)
+    localStorage.setItem("videoplayer-current-time", time.seconds)   
 }), 1000);
 
-player.setCurrentTime(localStorage.getItem("videoplayer-current-time"));
+const getItem = localStorage.getItem("videoplayer-current-time");
+
+if (getItem === null) {
+    return;
+} else {   
+    player.setCurrentTime(localStorage.getItem("videoplayer-current-time")) 
+};
+
 
 
